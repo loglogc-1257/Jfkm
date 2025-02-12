c5aa62e07e822ccb0a04a25a57d04781c8eb62b6
loglogc-1257 <fadonougbosebastien7@gmail.com>
2025-02-12 15:38:02 +0100

    Create fact.js

---
+++ commands/fact.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+const fs = require('fs');
+
+const token = fs.readFileSync('token.txt', 'utf8').trim();
+
+module.exports = {
+  name: 'fact',
+  description: 'Obtenez un fait intéressant.',
+  async execute(senderId) {
+    try {
+      const { data } = await axios.get('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en');
+      await sendMessage(senderId, { text: `📢 **Fait du jour** : ${data.text}` }, token);
+    } catch (error) {
+      console.error(error);
+      await sendMessage(senderId, { text: '❌ Impossible de récupérer un fait.' }, token);
+    }
+  }
+};
